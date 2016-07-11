cf10f31c743f4d61e75a039a3615dcf214ba2668
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-07-11 17:00:04 +0300

    implemented basic failure handling in java

--- week1/src/main/java/com/yakovchuk/App.java
+++ week1/src/main/java/com/yakovchuk/App.java
@@ -1,19 +1,47 @@
 package com.yakovchuk;
 
+import java.io.File;
+
 public class App {
 
-    public static final String MESSAGE_HELP = "expected input \"-[fh] [file name] [command] [all other]\"\n";
+    public static final String MESSAGE_HELP = "expected input \"-[fh] [file name] [command] [command args]\"\n";
+    public static final String MESSAGE_COMMAND_LIST = "command list:\n  list\n  add [text]\n  remove [task number]\n  remove all\n";
     public static final String MESSAGE_FAILURE_INVALID_INPUT = "failure: invalid input\n";
     public static final String OPTION_HELP = "-h";
+    public static final String OPTION_FILE = "-f";
 
-    public static void main(String [] args){
-        String option = args[0];
-        if ((option == null) || !(OPTION_HELP.equals(option))) {
-            outputError(MESSAGE_FAILURE_INVALID_INPUT);
-            outputHelp();
-            System.exit(0);
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                invalidInput();
+            } else if (OPTION_HELP.equals(args[0])) {
+                outputHelp();
+                System.exit(0);
+            } else if (OPTION_FILE.equals(args[0])) {
+                String filename = args[1];
+                if (filename == null) {
+                    invalidInput();
+                } else {
+                    File file = new File(filename);
+                    if (file.exists()) {
+
+                    } else {
+                        outputError("failure: file \"" + filename + "\" does not exist\n");
+                    }
+                }
+            } else {
+                invalidInput();
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            invalidInput();
         }
+
+    }
+
+    private static void invalidInput() {
+        outputError(MESSAGE_FAILURE_INVALID_INPUT);
         outputHelp();
+        System.exit(0);
     }
 
     private static void outputError(String message) {
@@ -22,6 +50,7 @@ public class App {
 
     private static void outputHelp() {
         System.out.print(MESSAGE_HELP);
+        System.out.print(MESSAGE_COMMAND_LIST);
     }
 
 }
--- week1/src/test/sh/test-app.sh
+++ week1/src/test/sh/test-app.sh
@@ -8,25 +8,41 @@ mkdir $TMP
 tf=$TMP/1 #test first
 ts=$TMP/2 #test second
 
-#01-print-help.sh
+#01-empty-input.sh
+
+sh $APP_SH > $tf
+java -jar $APP_JAVA > $ts
+
+printf "test 1 - empty input - diff "
+diff $tf $ts | wc -l
+rm $tf $ts
+
+
+#02-print-help.sh
 
 sh $APP_SH -h > $tf
 java -jar $APP_JAVA -h > $ts
 
-printf "test 1 - print help - diff "
+printf "test 2 - print help - diff "
 diff $tf $ts | wc -l
 rm $tf $ts
 
-#02-unexpected-option.sh
+#03-unexpected-option.sh
 
 sh $APP_SH -x > $tf
 java -jar $APP_JAVA -x > $ts
 
-printf "test 2 - unexpected option - diff "
+printf "test 3 - unexpected option - diff "
 diff $tf $ts | wc -l
 rm $tf $ts
 
+#04-file-not-exits.sh
 
+sh $APP_SH -f not-exist > $tf
+java -jar $APP_JAVA -f not-exist > $ts
 
+printf "test 4 - file not exist - diff "
+diff $tf $ts | wc -l
+#rm $tf $ts
 
-rm -r $TMP
+#rm -r $TMP

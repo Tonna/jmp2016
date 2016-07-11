4479e0f55b53102ba34f61055f72396d188ea51d
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-07-11 13:48:38 +0300

    implemented invalid option/input case

--- week1/src/main/java/com/yakovchuk/App.java
+++ week1/src/main/java/com/yakovchuk/App.java
@@ -2,8 +2,26 @@ package com.yakovchuk;
 
 public class App {
 
+    public static final String MESSAGE_HELP = "expected input \"-[fh] [file name] [command] [all other]\"\n";
+    public static final String MESSAGE_FAILURE_INVALID_INPUT = "failure: invalid input\n";
+    public static final String OPTION_HELP = "-h";
+
     public static void main(String [] args){
-        System.out.print("expected input \"-[fh] [file name] [command] [all other]\"\n");
+        String option = args[0];
+        if ((option == null) || !(OPTION_HELP.equals(option))) {
+            outputError(MESSAGE_FAILURE_INVALID_INPUT);
+            outputHelp();
+            System.exit(0);
+        }
+        outputHelp();
+    }
+
+    private static void outputError(String message) {
+        System.out.print(message);
+    }
+
+    private static void outputHelp() {
+        System.out.print(MESSAGE_HELP);
     }
 
 }

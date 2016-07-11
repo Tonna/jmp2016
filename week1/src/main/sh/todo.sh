530feb7dbe9654d44566808b745770124ed2526b
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-07-11 16:07:22 +0300

    reworked input handling

--- week1/src/main/sh/todo.sh
+++ week1/src/main/sh/todo.sh
@@ -6,37 +6,40 @@ COMMAND=$3
 INPUT=$4
 
 print_help() {
-  printf "expected input \"-[fh] [file name] [command] [all other]\"\n"
+  printf "expected input \"-[fh] [file name] [command] [command args]\"\n"
+  printf "command list:\n  list\n  add [text]\n  remove [task number]\n  remove all\n"
 }
 
-validate_remove_argument(){
-  if [ `echo $@ | grep "^[1-9][0-9]*$"` = '' ]
+print_invalid_input_message(){
+  printf "failure: invalid input\n"; print_help; 
+}
+
+check_is_number(){
+  if [ -z `echo $@ | grep "^[1-9][0-9]*$"` ]
     then printf "failure: input \"$@\" doesn't match number\n"; exit;
   fi
 }
 
-validate_input_non_empty(){
-  if [ -z $INPUT ]
-    then printf "failure: input is empty\n"; exit;
+check_non_empty(){
+  if [ -z $@ ]
+    then print_invalid_input_message; exit;
   fi
 }
 
 case "$OPTION" in
 -h) print_help; exit;;
--f) ;;
-*)  printf "failure: invalid input\n"; print_help; exit;;
+-f) check_non_empty $FILE;;
+*)  print_invalid_input_message; exit;
 esac
 
-
-if [ -z "$FILE" ] || test ! -f $FILE ;
-then
-  printf "failure: file \"$FILE\" does not exist\n"; exit
+if test ! -f $FILE
+  then printf "failure: file \"$FILE\" does not exist\n"; exit
 fi 
 
 case "$COMMAND" in
 list) awk '{ print NR " " $0 }' $FILE;;
-add) validate_input_non_empty; echo "$*" | cut -d ' ' -f 4- >> $FILE;;
-remove) validate_input_non_empty; validate_remove_argument $INPUT; sed -i "$INPUT"d $FILE;;
+add) check_non_empty $INPUT; echo "$*" | cut -d ' ' -f 4- >> $FILE;;
+remove) check_non_empty $INPUT; check_is_number $INPUT; sed -i "$INPUT"d $FILE;;
 remove-all) printf "" > $FILE;;
-*) printf "failure: unknown command \"$COMMAND\"\n"; exit;;
+*) print_invalid_input_message; exit;; 
 esac

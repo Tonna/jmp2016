c8ea072df984fea216ebce6e193dd43705afed22
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-07-14 17:00:04 +0300

    fix typo

--- week1/src/main/java/com/yakovchuk/UserOutputHelper.java
+++ week1/src/main/java/com/yakovchuk/UserOutputHelper.java
@@ -5,7 +5,7 @@ import java.io.PrintStream;
 class UserOutputHelper {
 
     private static final String MESSAGE_HELP = "expected input \"-[fh] [file name] [command] [command args]\"";
-    private static final String MESSAGE_COMMAND_LIST = "command list:\n  list\n  add [text]\n  remove [task number]\n  remove all";
+    private static final String MESSAGE_COMMAND_LIST = "command list:\n  list\n  add [text]\n  remove [task number]\n  remove-all";
     private static final String MESSAGE_FAILURE_INVALID_INPUT = "failure: invalid input";
     private static final String MESSAGE_FAILURE_INVALID_FILE = "failure: invalid file \"%s\"";
     private static final String MESSAGE_FAILURE_INVALID_NUMBER = "failure: input \"%s\" is not a number";

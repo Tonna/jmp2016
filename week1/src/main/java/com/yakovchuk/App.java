df471d48cbedb42c54b22a249f76afdbb85ff9be
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-07-14 12:55:39 +0300

    removed newlines from strings

--- week1/src/main/java/com/yakovchuk/App.java
+++ week1/src/main/java/com/yakovchuk/App.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class App {
 
-    private static final String MESSAGE_HELP = "expected input \"-[fh] [file name] [command] [command args]\"\n";
-    private static final String MESSAGE_COMMAND_LIST = "command list:\n  list\n  add [text]\n  remove [task number]\n  remove all\n";
-    private static final String MESSAGE_FAILURE_INVALID_INPUT = "failure: invalid input\n";
+    private static final String MESSAGE_HELP = "expected input \"-[fh] [file name] [command] [command args]\"";
+    private static final String MESSAGE_COMMAND_LIST = "command list:\n  list\n  add [text]\n  remove [task number]\n  remove all";
+    private static final String MESSAGE_FAILURE_INVALID_INPUT = "failure: invalid input";
     private static final String OPTION_HELP = "-h";
     private static final String OPTION_FILE = "-f";
     private static final String COMMAND_LIST = "list";
     private static final String COMMAND_ADD = "add";
-    private static final String COMMAD_REMOVE = "remove";
+    private static final String COMMAND_REMOVE = "remove";
     private static final String COMMAND_REMOVE_ALL = "remove-all";
 
     public static void main(String[] args) {
@@ -30,17 +30,17 @@ public class App {
                     if (COMMAND_LIST.equals(command)) {
                         List<String> lines = dao.list();
                         for (int i = 0; i < lines.size(); i++) {
-                            System.out.print(i + 1 + " " + lines.get(i) + "\n");
+                            System.out.println(i + 1 + " " + lines.get(i));
                         }
                     } else if (COMMAND_ADD.equals(command)) {
                         String newLine = Util.join(Arrays.asList(args).subList(3, args.length), " ");
                         dao.add(newLine);
-                    } else if (COMMAD_REMOVE.equals(command)) {
+                    } else if (COMMAND_REMOVE.equals(command)) {
                         Integer taskNum = null;
                         try {
                             taskNum = Integer.decode(args[3]) - 1;
                         } catch (NumberFormatException e) {
-                            outputError("failure: input \"" + args[3] + "\" is not a number\n");
+                            outputError("failure: input \"" + args[3] + "\" is not a number");
                             System.exit(0);
                         }
                         dao.remove(taskNum);
@@ -50,7 +50,7 @@ public class App {
                         invalidInput();
                     }
                 } catch (Exception e) {
-                    outputError("failure: invalid file \"" + filename + "\"\n");
+                    outputError("failure: invalid file \"" + filename + "\"");
                 }
             } else {
                 invalidInput();
@@ -67,12 +67,12 @@ public class App {
     }
 
     private static void outputError(String message) {
-        System.out.print(message);
+        System.out.println(message);
     }
 
     private static void outputHelp() {
-        System.out.print(MESSAGE_HELP);
-        System.out.print(MESSAGE_COMMAND_LIST);
+        System.out.println(MESSAGE_HELP);
+        System.out.println(MESSAGE_COMMAND_LIST);
     }
 
 }

7cecd589c5c5c0cd88854e0df414768b3d03a016
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-07-31 17:37:05 +0300

    Sample app for gc profiling. v2

--- gc/src/main/java/com/yakovchuk/MainGC.java
+++ gc/src/main/java/com/yakovchuk/MainGC.java
@@ -2,36 +2,67 @@ package com.yakovchuk;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
+
+import static java.lang.Thread.sleep;
 
 public class MainGC {
+
+    private static final int TEN_MINUTES = 10 * 60 * 60 * 1000;
+    private static final int TEN_SECONDS = 10 * 1000;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()) {
+        final Integer load = Integer.valueOf(args[0]);
+        final Integer interval = Integer.valueOf(args[1]);
+        final Integer threads = Integer.valueOf(args[2]);
 
-            Integer decoded = Integer.decode(scanner.next());
-            runSttuff(decoded);
-        }
-    }
-
-    private static void runSttuff(final Integer decoded) {
-        System.out.println("Add stuff " + decoded);
-
-        Runnable runnable = new Runnable() {
+        final Runnable job = new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < decoded; i++) {
-                    List list = new LinkedList<Object>();
+                for (int i = 0; i < load; i++) {
+                    List<String> list = new LinkedList<>();
                     for (int j = 0; j < 1000; j++) {
-                        list.add(new Object());
+                        list.add(String.valueOf(System.currentTimeMillis()));
                     }
                 }
             }
         };
-        new Thread(runnable).start();
-        new Thread(runnable).start();
-        new Thread(runnable).start();
-        new Thread(runnable).start();
-        new Thread(runnable).start();
+
+        Runnable scheduler = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        sleep(interval);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    for (int i = 0; i < threads; i++) {
+                        new Thread(job).start();
+                    }
+                }
+
+            }
+        };
+
+        Runnable stopper = new Runnable() {
+            @Override
+            public void run() {
+                long endTime = System.currentTimeMillis() + TEN_MINUTES;
+                while (System.currentTimeMillis() < endTime) {
+                    try {
+                        sleep(TEN_SECONDS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("stopping");
+                System.exit(0);
+            }
+        };
+
+        new Thread(scheduler).start();
+        new Thread(stopper).start();
+
     }
+
 }

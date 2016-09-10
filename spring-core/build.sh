9c2f65e0325d6ccc9406cb021746d417e55d5eae
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-09-10 22:35:38 +0300

    added hints for future

--- spring-core/build.sh
+++ spring-core/build.sh
@@ -3,7 +3,7 @@ mkdir ./target
 mkdir ./target/classes
 
 du -a | cut -f 2 | grep "/src/main/java.*java" | xargs javac -cp "./lib/*" -d ./target/classes 
-jar cfe ./target/spring-task.jar com/yakovchuk/SpringCoreMain -C ./target/classes . -C ./src/main/resources/ .
+jar cf ./target/spring-task.jar -C ./target/classes . -C ./src/main/resources/ .
 
 #libs used
 # commons-logging-1.2.jar
@@ -11,3 +11,6 @@ jar cfe ./target/spring-task.jar com/yakovchuk/SpringCoreMain -C ./target/classe
 # spring-context-4.3.2.RELEASE.jar
 # spring-core-4.3.2.RELEASE.jar
 # spring-expression-4.3.2.RELEASE.jar
+
+#run with
+#java -cp "./target/spring-task.jar;./lib/*" com.yakovchuk.SpringCoreMain

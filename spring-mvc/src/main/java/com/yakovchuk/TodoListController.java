f6622a3d5f2bc285b0463d3f63e3f29dc475e027
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-09-25 18:25:33 +0300

    new task can be created

--- spring-mvc/src/main/java/com/yakovchuk/TodoListController.java
+++ spring-mvc/src/main/java/com/yakovchuk/TodoListController.java
@@ -11,9 +11,28 @@ import java.util.List;
 @Controller
 public class TodoListController {
 
+    private static final String TODO_LIST_IN_SESSION = "todoList";
+
     @RequestMapping("/list")
     public String listTasks(HttpSession session, Model model) {
-        model.addAttribute("todoList", session.getAttribute("todoList"));
+        model.addAttribute("todoList", session.getAttribute (TODO_LIST_IN_SESSION));
         return "listOfTasks";
+    }
+
+    @RequestMapping("/addTaskForm")
+    public String addTaskForm() {
+        return "addNewTask";
+    }
+
+    @RequestMapping("/addTask")
+    public String addTask(HttpSession session, Model model, final String
+            taskText) {
+        List<String> list = (List<String>) session.getAttribute(TODO_LIST_IN_SESSION);
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+        list.add(taskText);
+        session.setAttribute(TODO_LIST_IN_SESSION, list);
+        return "redirect:list";
     }
 }

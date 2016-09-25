39022d19b4b8d14455c1dd8b4f292fc0eaaf7c76
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-09-25 14:30:49 +0300

    added listing of todo list. Getting list from session

--- spring-mvc/src/main/java/com/yakovchuk/TodoListController.java
+++ spring-mvc/src/main/java/com/yakovchuk/TodoListController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class TodoListController {
 
     @RequestMapping("/list")
-    public String listTasks(Model model) {
-        model.addAttribute("task", "Buy coffee");
+    public String listTasks(HttpSession session, Model model) {
+        model.addAttribute("todoList", session.getAttribute("todoList"));
         return "listOfTasks";
     }
 }

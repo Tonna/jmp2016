0269335e4a4a817e2e7abdf21f13ca25f477b137
Anton Yakovchuk <anton_yakovchuk@epam.com>
2016-09-25 21:10:26 +0300

    implemented edit and delete actions

--- spring-mvc/src/main/java/com/yakovchuk/TodoListController.java
+++ spring-mvc/src/main/java/com/yakovchuk/TodoListController.java
@@ -3,6 +3,7 @@ package com.yakovchuk;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -15,7 +16,7 @@ public class TodoListController {
 
     @RequestMapping("/list")
     public String listTasks(HttpSession session, Model model) {
-        model.addAttribute("todoList", session.getAttribute (TODO_LIST_IN_SESSION));
+        model.addAttribute("todoList", getTodoList(session));
         return "listOfTasks";
     }
 
@@ -25,14 +26,49 @@ public class TodoListController {
     }
 
     @RequestMapping("/addTask")
-    public String addTask(HttpSession session, Model model, final String
-            taskText) {
-        List<String> list = (List<String>) session.getAttribute(TODO_LIST_IN_SESSION);
+    public String addTask(HttpSession session, Model model,
+                          final String taskText) {
+        List<String> list =
+                getTodoList(session);
         if (list == null) {
             list = new ArrayList<>();
         }
         list.add(taskText);
         session.setAttribute(TODO_LIST_IN_SESSION, list);
+        return "redirect:list";
+    }
+
+    private List<String> getTodoList(HttpSession session) {
+        List<String> list = (List<String>) session.getAttribute(TODO_LIST_IN_SESSION);
+        if (list == null || list.isEmpty()) {
+            return null;
+        } else {
+            return list;
+        }
+    }
+
+    @RequestMapping("/deleteTask")
+    public String deleteTask(HttpSession session,
+                             @RequestParam("id") int id) {
+        List<String> todoList = getTodoList(session);
+        todoList.remove(id);
+        return "redirect:list";
+    }
+
+    @RequestMapping("/editTaskForm")
+    public String editTaskForm(HttpSession session,
+                           @RequestParam("id") int id, Model model) {
+        List<String> todoList = getTodoList(session);
+        model.addAttribute("taskText", todoList.get(id));
+        return "editTask";
+    }
+
+    @RequestMapping("/editTask")
+    public String editTask(HttpSession session,
+                           @RequestParam("id") int id,
+                           @RequestParam("taskText") String taskText) {
+        List<String> todoList = getTodoList(session);
+        todoList.set(id,taskText);
         return "redirect:list";
     }
 }
